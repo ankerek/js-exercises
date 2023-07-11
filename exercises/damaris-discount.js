84110fbbf898e154bd76b5f06aeb13993c064f6f
ankerek <ankerek@gmail.com>
2023-07-11 10:59:56 +0200

    add first exercises

---
+++ exercises/damaris-discount.js
@@ -0,0 +1,14 @@
+/**
+ * Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount rounded to two decimal places.
+ * 
+ * Examples:
+ * 
+ * dis(1500, 50) -> 750
+ * dis(89, 20) -> 71.2
+ * dis(100, 75) -> 25
+ * 
+ */
+
+function dis() {
+  
+}
---
+++ exercises/damaris-grades.js
@@ -0,0 +1,32 @@
+/**
+ * Part 1.
+ * It's the end of the semester and you got your grades from three classes: Geometry, Algebra, and Physics.
+ * 
+ * Create a function that:
+ * Reads the grades of these 3 classes (Grades range from 0 - 10)
+ * Calculate the average of your grades
+ * 
+ * Example: Geometry = 6, Algebra = 7, Physics = 8
+ * 
+ *          calculateAverage(6,7,8) -> 7
+ * 
+ * -------------
+ * 
+ * Part 2.
+ * 
+ * The exercise is almost identical to a previous exercise with a minor change. 
+ * It's the end of the semester and you got your marks from, Geometry, Algebra, Physics classes. 
+ * If the average score is 7 and above print "Good job!", 
+ * if the average score is between 6 and 4 print "You need to work harder!", 
+ * if the average score is below 4 print "Failed, you really need to work harder!".
+ * 
+ * Example: Geometry = 6, Algebra = 7, Physics = 8
+ * calculageAverage(6,7,8) -> Your average score is 7, Good job!"
+ *
+ */
+
+function calculateAverage() {
+  
+}
+
+
